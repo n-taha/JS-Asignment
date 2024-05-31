1db7035eeb34aaff1730ec9cb4bf5f84725db2cf
Mubtasim Ahsan Taha <mubtasimtaha@gmail.com>
2024-05-31 19:59:21 +0600

    Add files via upload

---
+++ name.js
@@ -0,0 +1,101 @@
+// 1.Create a variable called carName, assign the value Volvo to it.
+
+let carName = "Volvo" ; 
+
+// 2.On one single line, declare three variables with the following names and values:
+
+firstName = "John"
+lastName = "Doe"
+age = 35
+
+/*So What Will Be The 1st Variable Name, Then 2nd Variable Values & 
+3rd Variable Name And Values Both!*/
+
+let firstName = "John";    // write variable name
+let lastName = "Doe"    //write variable values 
+let age = 35 ;      // write variable name and values both 
+
+
+/* 3.Use the correct assignment operator that will 
+result in x being 50 (same as x = x * y).*/ 
+
+x = 10;
+y = 5;
+x = 50 // what will be the value of x ? 
+
+
+//4.Use comments to describe the correct data type of the following variables:
+
+let length = 16; // data type = "Number" 
+let lastName = "Johnson"; // data type =  "String"
+
+const x = {
+  firstName: "John",  
+  lastName: "Doe"
+};     // data type = "object" 
+
+//5.Execute the function named myFunction.
+ 
+function myFunction() {
+    alert("Hello World!");
+}
+
+
+myFunction();
+  
+  // call the function and see the output 
+  
+
+
+//6.
+
+
+const person = {
+    name: "John",
+    age: 50
+};
+
+
+alert(person.name + " is " + person.age);
+
+//7.
+
+const button = document.getElementById("nTaha");
+
+
+button.addEventListener("click", function() {
+    alert("This Is Mubtasim Ahsan Taha");
+});
+
+//8.
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length);
+
+const Brand = ["Volvo", "Jeep", "Mercedes"];
+Brand[0] = "Ford";
+console.log(Brand); 
+
+//9.
+const number = Math.number();
+console.log(number); //
+
+const largest_Number = Math.max(10, 20);
+console.log(largest_Number);  // Output Will Be "20";
+
+const squareRoot = Math.sqrt(9);
+console.log(squareRoot); // Output Will Be: "3";
+
+
+//10.
+
+let x = 10;
+let y = 5;
+
+alert(x > y); // Output Will be: "true";
+
+
+let age = 17; 
+
+ alert(age <= 18 ? "Too young" : "Old enough"); 
+
+
